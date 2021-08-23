8aaa10c4aad57e6a7504c3c70b96d632f181dfac
bokutotu <mushin.hudoushin@gmail.com>
2021-08-23 18:17:06 +0900

    Shortened the time it takes to test.

--- tests/test_epoch.py
+++ tests/test_epoch.py
@@ -7,6 +7,7 @@ import numpy as np
 from src.model import CGnet
 from src.data_module import LSTMDataset
 
+
 def test_epoch():
     coord = np.load("./tests/data/ala2_coordinates.npy")
     force = np.load("./tests/data/ala2_forces.npy")
@@ -19,7 +20,6 @@ def test_epoch():
         sampler=sampler.RandomSampler(dataset)
     )
 
-
     conf = {
         "is_angle_prior": True,
         "is_length_prior": True,
@@ -28,25 +28,30 @@ def test_epoch():
         "models": {
             "_target_": "src.layers.lstm.LSTM",
             "num_layers": 5,
-            "dropout": 0.0, 
+            "dropout": 0.0,
             "hidden_size": 256,
             "output_size": 1
         }
     }
 
     conf = OmegaConf.create(conf)
-    net = CGnet(conf, 5, torch.rand(3), torch.rand(3), 
-        torch.rand(2 * (5 - 3)), torch.rand(2 * (5 - 3)), 
-        torch.rand(4), torch.rand(4))
+    net = CGnet(conf, 5, torch.rand(3), torch.rand(3),
+                torch.rand(2 * (5 - 3)), torch.rand(2 * (5 - 3)),
+                torch.rand(4), torch.rand(4))
 
     optim = torch.optim.SGD(net.parameters(), lr=0.001)
     loss_func = torch.nn.MSELoss()
 
+    batch_counter = 0
+
     for (idx, batch) in enumerate(dataloader):
+        if batch_counter == 5:
+            break
         optim.zero_grad()
         x, y = batch
         print(x.size())
-        out,_ = net(x)
+        out, _ = net(x)
         loss = loss_func(out, y)
         loss.backward()
         optim.step()
+        batch_counter += 1
