9c212c142ae5fc6380c02f278c740e65ee1705b8
bokutotu <mushin.hudoushin@gmail.com>
2021-08-26 02:23:07 +0900

    fix bug

--- src/layers/mlp.py
+++ src/layers/mlp.py
@@ -8,9 +8,7 @@ class MLP(nn.Module):
         activate_function
     ):
         super().__init__()
-
         self.net = nn.Sequential()
-
         for i in range(num_layers-1):
             in_features = hidden_size if i != 0 else input_size
             self.net.add_module("layer_{}".format(i+1),
@@ -18,8 +16,7 @@ class MLP(nn.Module):
                                           out_features=hidden_size, ))
             self.net.add_module("activation_{}".format(i+1),
                                 getattr(nn, activate_function)())
-
-        self.net.add_module("layer_{}".format(i+1),
+        self.net.add_module("layer_{}".format(num_layers),
                             nn.Linear(in_features=hidden_size, out_features=output_size))
 
     def forward(self, x):
