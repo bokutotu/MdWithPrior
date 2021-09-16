0e7c5c6fdcf8fe3ec7b4ebb81e6093b992c31d40
bokutotu <mushin.hudoushin@gmail.com>
2021-09-17 02:33:10 +0900

    update load to use pytorc_lightning

--- src/load.py
+++ src/load.py
@@ -1,16 +1,30 @@
+import os
 from pathlib import Path
+from glob import glob
+from urllib.parse import urlparse
+from collections import OrderedDict
 
 import torch
 from omegaconf import OmegaConf
+import mlflow
 
 from src.setup import setup_model
+from src.experiment import Experiment
+
+
+def get_artifact(run_id):
+    uri = mlflow.get_run(run_id).info.artifact_uri
+    path = urlparse(uri).path
+    local_path = "/".join(path.split("/")[0:-1])
+    return local_path
 
 
 def load_from_run_id(run_id):
-    current_dir = Path.cwd()
-    artifact_dir = current_dir / "mlruns/1/{}/artifacts".format(run_id)
-    config_yaml = OmegaConf.load(str(artifact_dir / "config.yaml"))
-    model = setup_model(config_yaml)
-    state_dict = torch.load(str(artifact_dir / "model.pth"))
+    artifacts_path = get_artifact(run_id)
+    config_path = os.path.join(artifacts_path, "artifacts/config.yaml")
+    model_path = os.path.join(artifacts_path, "artifacts/model.pth")
+    config = OmegaConf.load(config_path)
+    state_dict = torch.load(model_path)
+    model = setup_model(config)
     model.load_state_dict(state_dict)
     return model
