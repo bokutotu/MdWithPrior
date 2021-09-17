31e064e33adacc5d5789c7cb8d87314392673cb4
bokutotu <mushin.hudoushin@gmail.com>
2021-09-18 03:10:30 +0900

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.experiment import Experiment
 import hydra
 from omegaconf import DictConfig
 from mlflow.pytorch import save_model
-# from src.train import train
+from src.train import train
 
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -11,11 +11,9 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 @hydra.main(config_path="configs/", config_name="config.yaml")
 def main(config: DictConfig) -> None:
-    exp = Experiment(config)
-    exp.run()
-    # exp.save_output()
-    # save_model(exp.model, "model")
-    # train(config)
+    # exp = Experiment(config)
+    # exp.run()
+    train(config)
 
 
 if __name__ == "__main__":
