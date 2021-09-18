b28ba405d91161ad63702213c846447450b280a9
bokutotu <mushin.hudoushin@gmail.com>
2021-09-18 23:46:20 +0900

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from src.train import train
 
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
 
 
 @hydra.main(config_path="configs/", config_name="config.yaml")
